cd5b02dac3b9ffcad539082400167a6949ed3341
秉虎 <s96016641@gmail.com>
2020-05-21 01:01:38 +0800

    Use type instead of array
    Implement quick sort

--- main.c
+++ main.c
@@ -2,13 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define INPUT_SIZE 30
+#define INPUT_SIZE 1000
 
-void printArray(const int *array, int arraySize) {
-    for (int i = 0; i < arraySize; i++) {
-        printf("%d ", *(array + i));
+#define SIZE 0
+#define MAX 1
+#define MIN 2
+
+#define START 0
+#define STOP 1
+
+typedef struct array {
+    int *item;
+    int size;
+} tArray;
+
+void printArray(tArray *array) {
+    for (unsigned int i = 0; i < array->size; i++) {
+        printf("%d ", array->item[i]);
     }
     printf("\n");
+}
+
+void swap(int *x, int *y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
 }
 
 int compInc(int x, int y) {
@@ -19,88 +37,116 @@ int compDec(int x, int y) {
     return y > x;
 }
 
-void bubbleSort(int *array, int arraySize, int (*Compare)(int, int)) {
+void bubbleSort(tArray *array, int (*Compare)(int, int)) {
     /* https://github.com/allen0099/advancedC/blob/week14/main.c */
-    for (int i = 0; i < arraySize - 1; i++) {
-        for (int j = 0; j < arraySize - 1 - i; j++) {
-            if (Compare(*(array + j), *(array + j + 1))) {
-                array[j] ^= array[j + 1] ^= array[j] ^= array[j + 1];
+    for (int i = 0; i < array->size - 1; i++) {
+        for (int j = 0; j < array->size - 1 - i; j++) {
+            if (Compare(array->item[j], array->item[j + 1])) {
+                swap(&array->item[j], &array->item[j + 1]);
             }
         }
     }
 }
 
-void exchange_sort(int *array, int arraySize) {
+void exchange_sort(tArray *array) {
     /* Same as bubble Sort */
-    for (int i = 0; i < arraySize - 1; i++) {
-        for (int j = 0; j < arraySize - 1 - i; j++) {
-            if (compInc(*(array + j), *(array + j + 1))) {
-                array[j] ^= array[j + 1] ^= array[j] ^= array[j + 1];
+    for (int i = 0; i < array->size - 1; i++) {
+        for (int j = 0; j < array->size - 1 - i; j++) {
+            if (compInc(array->item[j], array->item[j + 1])) {
+                swap(&array->item[j], &array->item[j + 1]);
             }
-            printArray(array, arraySize);
+            printArray(array);
         }
     }
 }
 
-void merge(int frontArraySize, int rearArraySize,
-           const int *frontArray, const int *rearArray, int *array) {
+void merge(tArray *front, tArray *rear, tArray *array) {
     /* input two sorted arrays, and merge them into one sorted array */
     int countFront = 0, countRear = 0, countArray = 0;
-    while (countFront < frontArraySize && countRear < rearArraySize) {
-        if (frontArray[countFront] < rearArray[countRear]) {
-            array[countArray++] = frontArray[countFront];
+    while (countFront < front->size && countRear < rear->size) {
+        if (front->item[countFront] < rear->item[countRear]) {
+            array->item[countArray++] = front->item[countFront];
             countFront++;
         } else {
-            array[countArray++] = rearArray[countRear];
+            array->item[countArray++] = rear->item[countRear];
             countRear++;
         }
     }
     /* Copy the rest into array */
-    if (countFront == frontArraySize) {
-        for (int i = countRear; i < rearArraySize; i++) {
-            array[countArray++] = rearArray[i];
+    if (countFront == front->size) {
+        for (int i = countRear; i < rear->size; i++) {
+            array->item[countArray++] = rear->item[i];
         }
     } else {
-        for (int i = countFront; i < frontArraySize; i++) {
-            array[countArray++] = frontArray[i];
+        for (int i = countFront; i < front->size; i++) {
+            array->item[countArray++] = front->item[i];
         }
     }
-    printArray(array, frontArraySize + rearArraySize);
+    printArray(array);
 }
 
-void merge_sort(int *array, int arraySize) {
-    if (arraySize > 1) {
-        int frontArraySize = arraySize / 2;
-        int rearArraySize = arraySize - frontArraySize;
-        int *frontArray = (int *) malloc(sizeof(int) * frontArraySize);
-        int *rearArray = (int *) malloc(sizeof(int) * rearArraySize);
+void merge_sort(tArray *array) {
+    if (array->size > 1) {
+        int frontArraySize = array->size / 2;
+        int rearArraySize = array->size - frontArraySize;
+
+        tArray *front, *rear;
+        front = (tArray *) malloc(sizeof(tArray));
+        rear = (tArray *) malloc(sizeof(tArray));
+
+        front->item = (int *) malloc(sizeof(int) * frontArraySize);
+        front->size = (int) frontArraySize;
+        rear->item = (int *) malloc(sizeof(int) * rearArraySize);
+        rear->size = (int) rearArraySize;
+
         for (int i = 0; i < frontArraySize; i++)
-            frontArray[i] = array[i];
+            front->item[i] = array->item[i];
         for (int j = 0; j < rearArraySize; j++)
-            rearArray[j] = array[frontArraySize + j];
-        merge_sort(frontArray, frontArraySize);
-        merge_sort(rearArray, rearArraySize);
-        merge(frontArraySize, rearArraySize, frontArray, rearArray, array);
+            rear->item[j] = array->item[frontArraySize + j];
+        merge_sort(front);
+        merge_sort(rear);
+        merge(front, rear, array);
     }
 }
 
-void quick_sort() {
+void partition(tArray *array, tArray *left, tArray *right) {
+    int pivotItem = array->item[0];
+    int j = 0;
+    for (int i = 0; i < array->size; i++) {
+        if (array->item[i] < pivotItem) {
+            j++;
+            swap(&array->item[i], &array->item[j]);
+        }
+    }
+    swap(array->item, &array->item[j]);
 
+    left->item = array->item;
+    left->size = j;
+    right->item = array->item + j + 1;
+    right->size = array->size - j - 1;
 }
 
-void a_sort() {
+void quick_sort(tArray *array) {
+    /* pivot is the first of the array */
+    if (array->size <= 1) {
+        return;
+    }
+    tArray *left, *right;
+    left = (tArray *) malloc(sizeof(tArray));
+    right = (tArray *) malloc(sizeof(tArray));
+    partition(array, left, right);
+    quick_sort(left);
+    quick_sort(right);
 
-}
-
-void b_sort() {
-
+    printArray(array);
 }
 
 int main() {
-    double START, END;
-    long ARRAY_SIZE, MAX_NUM, MIN_NUM;
+    double TIME_SHIFT[2];
+    long CONST_NUM[3];
     long choose;
 
+    /* fgets storage define */
     char str[INPUT_SIZE];
     char *ptr;
 
@@ -108,26 +154,28 @@ int main() {
     srand(time(NULL));
 //    srand(5);
 
-//    int fa[5] = {1, 2, 3, 5, 5};
-//    int ra[6] = {1, 1, 2, 3, 5, 5};
-//    int *a = (int *) malloc(sizeof(int) * 11);
-
     printf("======= Basic =======\n");
     printf("Array size: ");
     fgets(str, INPUT_SIZE, stdin);
-    ARRAY_SIZE = strtol(str, &ptr, 10);
+    CONST_NUM[SIZE] = strtol(str, &ptr, 10);
 
     printf("Array max: ");
     fgets(str, INPUT_SIZE, stdin);
-    MAX_NUM = strtol(str, &ptr, 10);
+    CONST_NUM[MAX] = strtol(str, &ptr, 10);
 
     printf("Array min: ");
     fgets(str, INPUT_SIZE, stdin);
-    MIN_NUM = strtol(str, &ptr, 10);
+    CONST_NUM[MIN] = strtol(str, &ptr, 10);
 
     /* Initial array */
-    int *array;
-    array = (int *) malloc(sizeof(int) * ARRAY_SIZE);
+    tArray *array = (tArray *) malloc(sizeof(tArray));
+    array->item = (int *) malloc(sizeof(int) * CONST_NUM[SIZE]);
+    array->size = (int) CONST_NUM[SIZE];
+
+    /* Init array with random values */
+    for (int i = 0; i < array->size; i++) {
+        array->item[i] = rand() % (CONST_NUM[MAX] - CONST_NUM[MIN] + 1) + CONST_NUM[MIN];
+    }
 
     printf("======= Datatype =======\n");
     printf("(1) Sorted array\n");
@@ -139,72 +187,60 @@ int main() {
 
     switch (choose) {
         case 1: // sorted
-            for (int i = 0; i < ARRAY_SIZE; i++) {
-                *(array + i) = (int) (rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM);
-            }
-            bubbleSort(array, (int) ARRAY_SIZE, compInc);
+            bubbleSort(array, compInc);
             break;
         case 2: // reverse sorted
-            for (int i = 0; i < ARRAY_SIZE; i++) {
-                *(array + i) = (int) (rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM);
-            }
-            bubbleSort(array, (int) ARRAY_SIZE, compDec);
-            break;
-        case 3: // random
-            for (int i = 0; i < ARRAY_SIZE; i++) {
-                *(array + i) = (int) (rand() % (MAX_NUM - MIN_NUM + 1) + MIN_NUM);
-            }
+            bubbleSort(array, compDec);
             break;
         default:
-            printf("Choice unknown, exiting...\n");
-            return 0;
+            break;
     }
     printf("Original array:\n");
-    printArray(array, (int) ARRAY_SIZE);
+    printArray(array);
 
     printf("======= Algorithm =======\n");
     printf("(1) Exchange sort\n");
     printf("(2) Merge sort\n");
     printf("(3) Quick sort\n");
-    printf("(4)  sort\n");
-    printf("(5)  sort\n");
+    printf("(4) Counting sort\n");
+    printf("(5) Heap sort\n");
 
     fgets(str, INPUT_SIZE, stdin);
     choose = strtol(str, &ptr, 10);
 
     switch (choose) {
         case 1: // exchange sort
-            START = clock();
-            exchange_sort(array, (int) ARRAY_SIZE);
-            END = clock();
+            TIME_SHIFT[START] = clock();
+            exchange_sort(array);
+            TIME_SHIFT[STOP] = clock();
             break;
         case 2: // merge sort
-            START = clock();
-            merge_sort(array, (int) ARRAY_SIZE);
-            END = clock();
+            TIME_SHIFT[START] = clock();
+            merge_sort(array);
+            TIME_SHIFT[STOP] = clock();
             break;
         case 3: // quick sort
-            START = clock();
-            quick_sort();
-            END = clock();
+            TIME_SHIFT[START] = clock();
+            quick_sort(array);
+            TIME_SHIFT[STOP] = clock();
             break;
-        case 4: // a sort
-            START = clock();
-            a_sort(); // implement this
-            END = clock();
+        case 4: // counting sort
+            TIME_SHIFT[START] = clock();
+//            a_sort(); // implement this
+            TIME_SHIFT[STOP] = clock();
             break;
-        case 5: // b sort
-            START = clock();
-            b_sort(); // implement this
-            END = clock();
+        case 5: // heap sort
+            TIME_SHIFT[START] = clock();
+//            b_sort(); // implement this
+            TIME_SHIFT[STOP] = clock();
             break;
         default:
             printf("Choice unknown, exiting...\n");
             return 0;
     }
     printf("======= Time =======\n");
-    printf("CLOCKS_PER_SEC in Posix is 1000000, so multiply it by 1000\n");
-    printf("==> Execute time: %.3f seconds\n", (END - START) / CLOCKS_PER_SEC * 1000);
+    printf("CLOCKS_PER_SEC in Posix system is 1000000, so multiply it with 1000\n");
+    printf("==> Execute time: %.3f seconds\n", (TIME_SHIFT[STOP] - TIME_SHIFT[START]) / CLOCKS_PER_SEC * 1000);
     printf("==> Total time: %.3f seconds\n", (double) clock() / CLOCKS_PER_SEC * 1000);
 
     return 0;
